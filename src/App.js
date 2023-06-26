1cfa0bdc0d9897025cae1f035340ae50a4d12fb4
alanbacay02 <alan.social02@gmail.com>
2023-06-26 17:30:15 +0800

    adds tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Timer() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
+		<div>
+			<p className="text-4xl">This is a timer!</p>
 		</div>
 	);
 }
 
-export default App;
+
+export default function App() {
+	return (
+		<div className="App">
+			<Timer />
+		</div>
+	);
+}
